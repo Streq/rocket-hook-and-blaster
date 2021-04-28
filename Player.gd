d58f18bf4adcfa843eb2f23a7bb1333bd023551f
streq <strequena@gmail.com>
2021-04-28 09:52:44 -0300

    playing with physics a lil bit nothing big

---
+++ Player.gd
@@ -0,0 +1,19 @@
+extends RigidBody2D
+
+
+export var fuel_power := 1000
+var move_dir : Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _physics_process(delta):
+	move_dir = Vector2(
+			float(Input.is_action_pressed("ui_right")) - float(Input.is_action_pressed("ui_left")),
+			float(Input.is_action_pressed("ui_down")) - float(Input.is_action_pressed("ui_up"))
+		).normalized()
+	apply_central_impulse(move_dir * fuel_power * delta)
+	
