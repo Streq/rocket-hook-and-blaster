a9b70928de1ec745c9bbfae4fec8706eba3c0c52
streq <strequena@gmail.com>
2021-04-30 10:44:12 -0300

    explosion (right mouse click) to boost yourself

--- Player.gd
+++ Player.gd
@@ -7,7 +7,9 @@ export (PackedScene) var Bullet
 var move_dir : Vector2
 var aiming_at : Vector2
 var wanna_shoot : bool = false
+var wanna_jump : bool = false
 var can_shoot : bool = true
+var jump_force : float = 1500
 onready var spawn_point = position
 
 # Called when the node enters the scene tree for the first time.
@@ -23,14 +25,17 @@ func _process(delta):
 
 func _input(event):
 	if event is InputEventMouseButton:
+		aiming_at = (event.position - Vector2(get_viewport_rect().size) * 0.5).normalized()
 #		print("pressed:" + str(event.pressed))
 #		print("index:" + str(event.button_index))
 #		print("factor:" + str(event.factor))
 #		print("doubeclick:" + str(event.doubleclick))
 #		print("position:" + str(event.position))
-		aiming_at = (event.position - Vector2(get_viewport_rect().size) * 0.5).normalized()
-#		print(aiming_at)
-		wanna_shoot = event.pressed
+		match event.button_index:
+			1:
+				wanna_shoot = event.pressed 
+			2:
+				wanna_jump = event.pressed
 	elif event is InputEventMouseMotion:
 		aiming_at = (event.position - Vector2(get_viewport_rect().size) * 0.5).normalized()
 		
@@ -48,15 +53,27 @@ func _physics_process(delta):
 		wanna_shoot = false
 		if can_shoot:
 			shoot()
+	
+	# jump
+	if wanna_jump:
+		wanna_jump = false
+		jump()
+		
+func jump():
+	apply_central_impulse(-aiming_at * jump_force)
+	$Explosion.visible = true
+	$Explosion.rotation = (-aiming_at).angle()
+	yield(get_tree().create_timer(0.1),"timeout")
+	$Explosion.visible = false
 
 func shoot():
 	var b = Bullet.instance()
+	b.parent = self
 	owner.add_child(b)
 	b.position = position
 	b.rotation = aiming_at.angle()
 	apply_central_impulse(-aiming_at * b.recoil)
 	
-	wanna_shoot = false
 	can_shoot = false
 	$GunCooldown.start()
 
